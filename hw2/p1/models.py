244865ae9db30df815e7768c7c3062fcc7ba5204
r08922114 <r08922114@ntu.edu.tw>
2021-03-29 23:21:53 +0800

    add LSTM

--- hw2/p1/models.py
+++ hw2/p1/models.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 
 
+
+
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
@@ -37,44 +39,62 @@ class Classifier(nn.Module):
         return x
 
 
+class LSTMNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, is_bidirection=True, num_layers=4, dropout_rate=0.1):
+
+        super(LSTMNet, self).__init__()
+        self.hidden_dim    = hidden_dim
+        self.is_bidirection = is_bidirection
+        self.num_layers     = num_layers
+        
+        output_hidden_dim = self.hidden_dim * 2 if is_bidirection else self.hidden_dim
+        
+        self.lstm   = nn.LSTM(input_dim, self.hidden_dim, num_layers=num_layers, dropout=dropout_rate, bidirectional=is_bidirection)
+        self.linaer = nn.Linear(output_hidden_dim, 39) 
+
+    def forward(self, x):
+        batch_size = 
+
 # BLSTM for pBLSTM
 # Step 1: Reduce time resolution to half
 # Step 2: Run through BLSTM
 # Note the input should have timestamp%2 == 0
-class pBLSTMLayer(nn.Module):
-    def __init__(self, input_dim, hidden_dim, rnn_unit='LSTM', dropout_rate=0.0):
-        super(pBLSTMLayer, self).__init__()
-        self.rnn_unit = getattr(nn, rnn_unit.upper())
+# class pBLSTMLayer(nn.Module):
+#     def __init__(self, input_dim, hidden_dim, rnn_unit='LSTM', dropout_rate=0.0):
+#         super(pBLSTMLayer, self).__init__()
+#         self.rnn_unit = getattr(nn, rnn_unit.upper())
 
-        # feature dimension will be doubled since time resolution reduction
-        self.BLSTM = self.rnn_unit(input_dim*2, hidden_dim, 1, bidirectional=True, dropout=dropout_rate, batch_first=True)
+#         # feature dimension will be doubled since time resolution reduction
+#         self.BLSTM = self.rnn_unit(input_dim*2, hidden_dim, 1, bidirectional=True, dropout=dropout_rate, batch_first=True)
 
-    def forward(self, input):
-        batch_size = input.size(0)
-        timestep = input.size(1)
-        feature_dim = input.size(2)
+#     def forward(self, input):
+#         batch_size = input.size(0)
+#         timestep = input.size(1)
+#         feature_dim = input.size(2)
 
-        # Reduce time resolution
-        input = input.contiguous().view(batch_size, int(timestep/2), feature_dim*2)
+#         # Reduce time resolution
+#         input = input.contiguous().view(batch_size, int(timestep/2), feature_dim*2)
 
-        # Bidirectional RNN
-        hidden_state, cell_state = self.BLSTM(input)
-        return hidden_state, cell_state
+#         # Bidirectional RNN
+#         hidden_state, cell_state = self.BLSTM(input)
+#         return hidden_state, cell_state
 
 
-# Listener is a pBLSTM(pyramial Bi-LSTM) stacking 3 layer to reduce time resolution 8 times
-class Listener(nn.Module):
-    def __init__(self, input_dim, hidden_dim, listener_layer, rnn_unit, dropout_rate, **kwargs):
-        super(Listener, self).__init__()
+# # Listener is a pBLSTM(pyramial Bi-LSTM) stacking 3 layer to reduce time resolution 8 times
+# class Listener(nn.Module):
+#     def __init__(self, input_dim, hidden_dim, listener_layer, rnn_unit, dropout_rate, **kwargs):
+#         super(Listener, self).__init__()
 
-        # Listener RNN layer
-        self.listener_layer = listener_layer
-        assert self.listener_layer >= 1, 'Listener should have at least 1 layer'
+#         # Listener RNN layer
+#         self.listener_layer = listener_layer
+#         assert self.listener_layer >= 1, 'Listener should have at least 1 layer'
 
-        self.pLSTM_layer0 = pBLSTMLayer(input_dim, hidden_dim, rnn_unit=rnn_unit, dropout_rate=dropout_rate)
+#         self.pLSTM_layer0 = pBLSTMLayer(input_dim, hidden_dim, rnn_unit=rnn_unit, dropout_rate=dropout_rate)
 
-        for i in range(1, self.listener_layer):
-            setattr(self, 'pLSTM_layer'+str(i), pBLSTMLayer(hidden_dim*2, hidden_dim, rnn_unit=rnn_unit, dropout_rate=dropout_rate))
+#         for i in range(1, self.listener_layer):
+#             setattr(self, 'pLSTM_layer'+str(i), pBLSTMLayer(hidden_dim*2, hidden_dim, rnn_unit=rnn_unit, dropout_rate=dropout_rate))
+
+
 
 
 if __name__ == '__main__':
